4090053b6090c9d21d93241586f33d6a1675a4ad
Jonathan Perron <contact@jonathanperron.fr>
2023-12-02 10:35:58 +0100

    Setup repo (#2)
    * chore(setup): organise repo to follow good practices
    
    * See https://github.com/astronomer/airflow-provider-sample for details
    
    * chore(setup): fill pyproject.yaml properly
    
    * Add get_provider_info() function in __init__ file
    * Fill pyproject.yaml following recommendation
    
    * chore(setup): fill README

---
+++ airflow-provider-lokalise/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "1.0.0"
+
+## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-lokalise",  # Required
+        "name": "Lokalise",  # Required
+        "description": "Lokalise hook and operator for Airflow based on the Lokalise Python SDK.",  # Required
+        "connection-types": [
+            {
+                "connection-type": "lokalise",
+                "hook-class-name": "airflow-provider-lokalise.hooks.lokalise.LokaliseHook"
+            }
+        ],
+        "extra-links": ["airflow-provider-lokalise.operators.lokalise.LokaliseOperator"],
+        "versions": [__version__],  # Required
+    }
