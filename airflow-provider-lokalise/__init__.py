2da4c8624771418b24fc220c9c35a0bcb1e60466
Jonathan Perron <jonathan.perron@lumapps.com>
2023-12-02 10:12:10 +0100

    chore(setup): fill pyproject.yaml properly
    * Add get_provider_info() function in __init__ file
    * Fill pyproject.yaml following recommendation

--- airflow-provider-lokalise/__init__.py
+++ airflow-provider-lokalise/__init__.py
@@ -1,19 +1,17 @@
-from __future__ import annotations
+__version__ = "1.0.0"
 
-import packaging.version
-
-__all__ = ["__version__"]
-
-__version__ = "8.12.0"
-
-try:
-    from airflow import __version__ as airflow_version
-except ImportError:
-    from airflow.version import version as airflow_version
-
-if packaging.version.parse(packaging.version.parse(airflow_version).base_version) < packaging.version.parse(
-    "2.5.0"
-):
-    raise RuntimeError(
-        f"The package `apache-airflow-providers-amazon:{__version__}` needs Apache Airflow 2.5.0+"
-    )
+## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-lokalise",  # Required
+        "name": "Lokalise",  # Required
+        "description": "Lokalise hook and operator for Airflow based on the Lokalise Python SDK.",  # Required
+        "connection-types": [
+            {
+                "connection-type": "lokalise",
+                "hook-class-name": "airflow-provider-lokalise.hooks.lokalise.LokaliseHook"
+            }
+        ],
+        "extra-links": ["airflow-provider-lokalise.operators.lokalise.LokaliseOperator"],
+        "versions": [__version__],  # Required
+    }
