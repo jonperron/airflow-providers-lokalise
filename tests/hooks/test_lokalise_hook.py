1b0ada91676fd7c336483ecf5480c69f2acd35be
Jonathan Perron <jonathan.perron@lumapps.com>
2023-12-05 08:16:48 +0100

    chore(setup): prepare repo for pushing project
    * Update gitignore
    * Update pyproject
    * Rename repo to make it clearer on import
    * Fix import path
    * Fix naming convention not followed

--- tests/hooks/test_lokalise_hook.py
+++ tests/hooks/test_lokalise_hook.py
@@ -11,19 +11,21 @@ import logging
 import pytest
 from unittest.mock import Mock, patch
 
+from airflow.models import Connection
+from airflow.utils import db
 from lokalise import Client as LokaliseClient
 from lokalise.models.project import ProjectModel
 from lokalise.errors import Unauthorized
 
-from airflow.models import Connection
-from airflow.utils import db
-from provider.hooks.lokalise import LokaliseHook
+
+from lokalise_provider.hooks.lokalise import LokaliseHook
 
 log = logging.getLogger(__name__)
 
 pytestmark = pytest.mark.db_test
 
 lokalise_client_mock = Mock(name="lokalise_client_for_test")
+
 
 class TestLokaliseHook:
     def setup_class(self) -> None:
@@ -32,12 +34,14 @@ class TestLokaliseHook:
                 conn_id="lokalise_default",
                 conn_type="lokalise",
                 password="my-super-token",
-                host="default_project",                
+                host="default_project",
             )
         )
 
     @patch(
-        "provider.hooks.lokalise.LokaliseClient", autospec=True, return_value=lokalise_client_mock
+        "lokalise_provider.hooks.lokalise.LokaliseClient",
+        autospec=True,
+        return_value=lokalise_client_mock,
     )
     def test_lokalise_client_connection(self, lokalise_mock) -> None:
         lokalise_hook = LokaliseHook()
@@ -52,20 +56,17 @@ class TestLokaliseHook:
         hook.client.project.return_value = ProjectModel
 
         status, msg = hook.test_connection()
-        
+
         assert status is True
         assert msg == "Successfully connected to Lokalise."
 
     def test_connection_failure(self) -> None:
         hook = LokaliseHook()
         hook.client.project = Mock(
-            side_effect=Unauthorized(
-                code=401,
-                msg="API token is incorrect",
-            )
+            side_effect=Unauthorized(code=401, msg="API token is incorrect")
         )
 
         status, msg = hook.test_connection()
-        
+
         assert status is False
         assert msg == "('API token is incorrect', 401)"
