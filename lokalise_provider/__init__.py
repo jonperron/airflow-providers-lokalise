93afa709cac26c89ac4df1e1b653df3608673c00
Jonathan Perron <jonathan.perron@lumapps.com>
2023-12-08 10:59:21 +0100

    Feat(connection): add rendering of connection

--- lokalise_provider/__init__.py
+++ lokalise_provider/__init__.py
@@ -1,19 +1,16 @@
-__version__ = "1.0.1"
+__version__ = "1.1.3"
 
 ## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
 def get_provider_info():
     return {
-        "package-name": "airflow-provider-lokalise",  # Required
+        "package-name": "airflow-providers-lokalise",  # Required
         "name": "Lokalise",  # Required
         "description": "Lokalise hook and operator for Airflow based on the Lokalise Python SDK.",  # Required
         "connection-types": [
             {
                 "connection-type": "lokalise",
-                "hook-class-name": "airflow-provider-lokalise.hooks.lokalise.LokaliseHook",
+                "hook-class-name": "lokalise_provider.hooks.lokalise.LokaliseHook",
             }
-        ],
-        "extra-links": [
-            "airflow-provider-lokalise.operators.lokalise.LokaliseOperator"
         ],
         "versions": [__version__],  # Required
     }
